faaf1fb971e58b0e761f9c81ee0689873e7e2742
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-25 20:27:26 +0800

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,21 +1,13 @@
 import streamlit as st
 import cv2
-from matplotlib import pyplot as plt
 
-def detect_faces(image_path):
-    # Opening image
+def detect_faces(image_path, cascade_model):
     img = cv2.imread(image_path)
-
-    # OpenCV opens images as BGR but we want it as RGB
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Use minSize to ignore small detections
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    found = cascade_model.detectMultiScale(img_gray, minSize=(20, 20))
 
-    found = face_cascade.detectMultiScale(img_gray, minSize=(20, 20))
-
-    # Draw rectangles around detected faces
     for (x, y, w, h) in found:
         cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
@@ -31,6 +23,13 @@ def main():
 
     st.title("Face Detection")
 
+    cascade_model = None
+
+    cascade_file = st.file_uploader("Upload a Cascade Model (XML)", type=["xml"])
+
+    if cascade_file is not None:
+        cascade_model = cv2.CascadeClassifier(cv2.data.haarcascades + cascade_file.name)
+
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
@@ -38,12 +37,11 @@ def main():
         with open(image_path, "wb") as f:
             f.write(uploaded_file.read())
 
-        detected_image = detect_faces(image_path)
-        
-        image_width = st.slider("Adjust Image Width", min_value=100, max_value=800, value=500)
-
-        st.subheader("Original Image")
-        st.image(cv2.cvtColor(detected_image, cv2.COLOR_BGR2RGB), width=image_width)
+        if cascade_model:
+            detected_image = detect_faces(image_path, cascade_model)
+            image_width = st.slider("Adjust Image Width", min_value=100, max_value=800, value=500)
+            st.subheader("Original Image with Detected Faces")
+            st.image(cv2.cvtColor(detected_image, cv2.COLOR_BGR2RGB), width=image_width)
 
 if __name__ == "__main__":
     main()
