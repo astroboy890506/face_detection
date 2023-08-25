7caedb404feded753d064f751277ecb3f6232982
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-25 20:31:54 +0800

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import cv2
 from matplotlib import pyplot as plt
+import tempfile
+import os
 
 def detect_and_mark_objects(image_path, cascade_xml_path, min_size=(20, 20)):
     # Open the image
@@ -39,13 +41,20 @@ def main():
         with open(image_path, "wb") as f:
             f.write(uploaded_file.read())
 
-        cascade_xml_path = st.text_input("Enter the path to the cascade XML file")
+        cascade_xml_file = st.file_uploader("Upload the cascade XML file", type=["xml"])
 
-        if cascade_xml_path and st.button("Detect Objects"):
+        if cascade_xml_file and st.button("Detect Objects"):
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(cascade_xml_file.read())
+                cascade_xml_path = temp_file.name
+
             detected_image = detect_and_mark_objects(image_path, cascade_xml_path)
             image_width = st.slider("Adjust Image Width", min_value=100, max_value=800, value=500)
             st.subheader("Image with Detected Objects")
             st.image(detected_image, width=image_width)
+
+            # Clean up the temporary XML file
+            os.unlink(cascade_xml_path)
 
 if __name__ == "__main__":
     main()
